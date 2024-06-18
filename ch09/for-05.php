e3041baddfe9e3cec27043cbcc1236f1e407cfa4
GomCoder <k2001911@hanmail.net>
2024-06-18 18:34:51 +0900

    ch09 반복문

---
+++ ch09/for-05.php
@@ -0,0 +1,16 @@
+<?php
+    $cars = array(
+        array("Volvo", 10, 300),
+        array("BMW", 11, 250),
+        array("Saab", 12, 350),
+        array("Kia", 13, 200)
+    );
+
+    for ($i = 0; $i < 4; $i++) {
+        for ($j = 0; $j < 3; $j++) {
+            echo $cars[$i][$j];
+            echo " ";
+        }
+        echo "<br>";
+    }
+?>
---
+++ ch09/while-02.php
@@ -0,0 +1,14 @@
+<?php
+    $i = 0;
+
+
+    // while($i  <= 5) {
+    //     echo "The number is: $i <br>";
+    //     $i++;
+    // }
+
+    for (; $i < 5;) {
+        $i++;
+        echo "The number is: $i <br>";
+    }
+?>
